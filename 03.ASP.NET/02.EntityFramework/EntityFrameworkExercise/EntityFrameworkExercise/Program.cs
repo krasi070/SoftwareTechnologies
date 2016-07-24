cd734f7892aa0b6f98977811fa51f1c3677b997c
krasi070 <krasi0707@gmail.com>
2016-07-24 23:07:41 +0300

    added entity framework hw

---
+++ 03.ASP.NET/02.EntityFramework/EntityFrameworkExercise/EntityFrameworkExercise/Program.cs
@@ -0,0 +1,219 @@
+﻿namespace EntityFrameworkExercise
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //Problem01ListAllPosts();
+            //Problem02ListAllUsers();
+            //Problem03ListTitleAndBodyFromPosts();
+            //Problem04OrderData();
+            //Problem05OrderByTwoColumns();
+            //Problem06SelectAuthors();
+            //Problem07JoinAuthorWithTitle();
+            //Problem08SelectAuthorOfSpecificPost();
+            //Problem09OrderPostsAuthors();
+            //Problem10CreateData();
+            //Problem11UpdateData();
+            //Problem12DeleteComment();
+            //Problem13DeletePost();
+        }
+
+        private static void Problem01ListAllPosts()
+        {
+            var db = new BlogDbContext();
+            var posts = db.Posts.Select(p => p).ToList();
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine("Title: {0}", post.Title);
+                Console.WriteLine("Author ID: {0}", post.AuthorId);
+                Console.WriteLine("Comments Count: {0}", post.Comments.Count);
+                Console.WriteLine("Tags Count: {0}", post.Tags.Count);
+                Console.WriteLine("----------------------------------------------------");
+            }
+        }
+
+        private static void Problem02ListAllUsers()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users.Select(u => u).ToList();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("ID: {0}", user.Id);
+                Console.WriteLine("Name: {0}", user.FullName);
+                Console.WriteLine("Comments Count: {0}", user.Comments.Count);
+                Console.WriteLine("Posts Count: {0}", user.Posts.Count);
+                Console.WriteLine("-------------------------------------------------");
+            }
+        }
+
+        private static void Problem03ListTitleAndBodyFromPosts()
+        {
+            var db = new BlogDbContext();
+            var posts = db.Posts
+                .Select(p => new
+                {
+                    p.Title,
+                    p.Body
+                }).ToList();
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine("Title: {0}", post.Title);
+                Console.WriteLine("Content: {0}", post.Body);
+                Console.WriteLine("-------------------------------------------------------");
+            }
+        }
+
+        private static void Problem04OrderData()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users
+                .OrderBy(u => u.UserName)
+                .ToList();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("Full name: {0}", user.FullName);
+                Console.WriteLine("---------------------------------------------------");
+            }
+        }
+
+        private static void Problem05OrderByTwoColumns()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users
+                .OrderByDescending(u => u.UserName)
+                .ThenByDescending(u => u.FullName)
+                .ToList();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("Full name: {0}", user.FullName);
+                Console.WriteLine("---------------------------------------------------");
+            }
+        }
+
+        private static void Problem06SelectAuthors()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users
+                .Where(u => u.Posts.Count > 0)
+                .ToList();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("Full name: {0}", user.FullName);
+                Console.WriteLine("Posts Count: {0}", user.Posts.Count);
+                Console.WriteLine("-------------------------------------------------------------");
+            }
+        }
+
+        private static void Problem07JoinAuthorWithTitle()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users
+                .SelectMany(u => u.Posts, (u, p) => new {u.UserName, p.Title});
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("Post Title: {0}", user.Title);
+                Console.WriteLine("----------------------------------------------");
+            }
+        }
+
+        private static void Problem08SelectAuthorOfSpecificPost()
+        {
+            var db = new BlogDbContext();
+            var author = db.Users
+                .SelectMany(u => u.Posts, (u, p) => new {u.UserName, u.FullName, p.Id})
+                .Single(p => p.Id == 4);
+
+            Console.WriteLine("Username: {0}", author.UserName);
+            Console.WriteLine("Full name: {0}", author.FullName);
+        }
+
+        private static void Problem09OrderPostsAuthors()
+        {
+            var db = new BlogDbContext();
+            var users = db.Users
+                .Where(u => u.Posts.Count > 0)
+                .OrderByDescending(u => u.Id)
+                .ToList();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine("Username: {0}", user.UserName);
+                Console.WriteLine("Full name: {0}", user.FullName);
+                Console.WriteLine("----------------------------------------------------");
+            }
+        }
+
+        private static void Problem10CreateData()
+        {
+            var db = new BlogDbContext();
+            var post = new Posts()
+            {
+                Title = "Random Title",
+                Body = "Random Content",
+                Date = DateTime.Now,
+                AuthorId = 2
+            };
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+
+            Console.WriteLine("Post #{0} created!", post.Id);
+        }
+
+        private static void Problem11UpdateData()
+        {
+            var db = new BlogDbContext();
+            var user = db.Users
+                .Single(u => u.UserName == "GBotev");
+
+            string oldName = user.FullName;
+            user.FullName = "Georgi Botev";
+
+            db.SaveChanges();
+
+            Console.WriteLine("User \'{0}\' has been renamed to {1}.", oldName, user.FullName);
+        }
+
+        private static void Problem12DeleteComment()
+        {
+            var db = new BlogDbContext();
+            var comment = db.Comments.Single(c => c.Id == 1);
+
+            db.Comments.Remove(comment);
+
+            db.SaveChanges();
+
+            Console.WriteLine("Comment #{0} deleted!", comment.Id);
+        }
+
+        private static void Problem13DeletePost()
+        {
+            var db = new BlogDbContext();
+            var post = db.Posts.Single(p => p.Id == 31);
+
+            db.Comments.RemoveRange(post.Comments);
+            post.Tags.Clear();
+            db.Posts.Remove(post);
+
+            db.SaveChanges();
+
+            Console.WriteLine("Post #{0} deleted successfully!", post.Id);
+        }
+    }
+}
---
+++ 03.ASP.NET/02.EntityFramework/EntityFrameworkLab/BlogDbApp/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace BlogDbApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class PostData
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //Problem01QueryData();
+            //Problem02CreateNewData();
+            //Problem03CascadingInsert();
+            //Problem04UpdateExistingData();
+            //Problem05DeleteExistingData();
+            //Problem06ExecuteNativeSQL();
+        }
+
+        private static void Problem01QueryData()
+        {
+            var db = new BlogDbContext();
+            var posts = db.Posts.Select(p => new
+            {
+                p.Id,
+                p.Title,
+                Comments = p.Comments.Count,
+                Tags = p.Tags.Count
+            });
+
+            Console.WriteLine("SQL Query:\n{0}\n", posts);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine(
+                    "{0} {1} ({2} comments, {3} tags)",
+                    post.Id,
+                    post.Title,
+                    post.Comments,
+                    post.Tags);
+            }
+        }
+
+        private static void Problem02CreateNewData()
+        {
+            var db = new BlogDbContext();
+            var post = new Posts()
+            {
+                Title = "New Title",
+                Body = "New Post Body",
+                Date = DateTime.Now
+            };
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+            Console.WriteLine("Post #{0} created.", post.Id);
+        }
+
+        private static void Problem03CascadingInsert()
+        {
+            var db = new BlogDbContext();
+            var post = new Posts()
+            {
+                Title = "New Post Title",
+                Date = DateTime.Now,
+                Body = "This posts has comments and tags.",
+                Comments = new Comments[]
+                {
+                    new Comments() { Text = "Text 1", Date = DateTime.Now },
+                    new Comments() { Text = "Text 2", Date = DateTime.Now, Users = db.Users.First() } 
+                },
+                Tags = db.Tags.Take(3).ToList() 
+            };
+
+            db.Posts.Add(post);
+            db.SaveChanges();
+        }
+
+        private static void Problem04UpdateExistingData()
+        {
+            var db = new BlogDbContext();
+            var user = db.Users
+                .Where(u => u.UserName == "pesho")
+                .First();
+            user.PasswordHash = Guid.NewGuid().ToByteArray();
+
+            db.SaveChanges();
+            Console.WriteLine("User #{0} ({1}) has a new random password.", user.Id, user.UserName);
+        }
+
+        private static void Problem05DeleteExistingData()
+        {
+            var db = new BlogDbContext();
+            var lastPost = db.Posts
+                .OrderByDescending(p => p.Id)
+                .First();
+
+            db.Comments.RemoveRange(lastPost.Comments);
+            lastPost.Tags.Clear();
+            db.Posts.Remove(lastPost);
+            db.SaveChanges();
+
+            Console.WriteLine("Deleted post #{0}.", lastPost.Id);
+        }
+
+        private static void Problem06ExecuteNativeSQL()
+        {
+            var db = new BlogDbContext();
+            var startDate = new DateTime(2016, 5, 19);
+            var endDate = new DateTime(2016, 6, 14);
+
+            var posts = db.Database.SqlQuery<PostData>(
+                @"SELECT ID, Title, Date FROM Posts
+                WHERE CONVERT(date, DATE)
+                BETWEEN {0} AND {1}
+                ORDER BY Date", startDate, endDate);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine("#{0}: {1} ({2})", post.ID, post.Title, post.Date);
+            }
+        }
+    }
+}
