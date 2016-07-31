49318c08db501bf71ca98f8a3f58e5d19fbdcc95
krasi070 <krasi0707@gmail.com>
2016-07-31 23:23:49 +0300

    added asp.net mvc blog

---
+++ 03.ASP.NET/04.CreatingABlogWith ASP.NET MVC/Blog/Blog/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var posts = db.Posts.OrderByDescending(p => p.Date).Take(3);
+
+            return View(posts.ToList());
+        }
+    }
+}
---
+++ 03.ASP.NET/04.CreatingABlogWith ASP.NET MVC/Blog/Blog/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+           
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+    }
+}
